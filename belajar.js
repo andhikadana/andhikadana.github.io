37c3d76b4eb2c91d4ac31937c35674f12caf2fb6
Danaraja <danaraja2006@gmail.com>
2021-12-28 14:34:43 +0700

    Free

---
+++ belajar.js
@@ -0,0 +1,7 @@
+var semua = {
+    nama : "Andhika Aji Ardhana",
+    alamat : "Karangampel Rt03/Rw03",
+    usia : 15,
+    hobi : "memancing"
+}
+document.getElementById("keseluruhan").innerHTML = semua.nama + " tinggal Di " + semua.alamat + " Usia " + semua.usia +" Hobinya " + semua.hobi;
