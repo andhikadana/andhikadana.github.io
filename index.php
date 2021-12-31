a5ec97b48cf3a00d80bb49086e5dc855493cd554
Danaraja <danaraja2006@gmail.com>
2021-12-31 10:36:48 +0700

    pengetesan waktu

--- index.php
+++ index.php
@@ -10,6 +10,13 @@
 <body>
     <center>
     <h1>SELAMAT DATANG</h1>
+    <div class="col-lg-12 bg-dark" style="color: lime;" align='center'>
+        <?php
+
+        date_default_timezone_set('Asia/Jakarta');
+        echo 'Indonesian Timezone: ' . date('d-m-Y H:i:s');
+        ?>
+    </div>
     <div class="kotak">
         <h2>I Have Song For You</h2>
         <marquee><img src="rick.gif"><img src="rick.gif"><img src="rick.gif"><img src="rick.gif"><img src="rick.gif"></marquee>
@@ -20,9 +27,9 @@
         <a href="file.html"><button class="btn">In to  New File</button></a>
     </div>
     <br/>
-    <h1>CODE NAME</h1>
+    <h1>1</h1>
     <div align='center'>
-        <button class="btn" onclick="peringatan()"><h1>CODE NUK</h1></button>
+        <button class="btn" onclick="peringatan()"><h1>///</h1></button>
     </div>
         <div id='fungsi'></div>
     </body>
@@ -30,7 +37,7 @@
     function peringatan(){
         var attn = '';
         var n;
-        for(n = 0;n <=3;n++){
+        for(n = 9;n <=11;n++){
            attn += '<h1><center>SELAMAT GT-208!</center></h1>' + n + "<br/>";
         }
         document.getElementById('fungsi').innerHTML = attn;
